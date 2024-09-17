9229be356ddb575ec5be6b9d24a47c7260357691
Dileep Kumar Behera <126802368+Dipu2309@users.noreply.github.com>
2024-09-18 00:15:53 +0530

    homepage added

---
+++ Learn_Java_With_Dileep/src/main/java/com/java/dileep/LearnJavaWithDileepApplication.java
@@ -0,0 +1,13 @@
+package com.java.dileep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaWithDileepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJavaWithDileepApplication.class, args);
+	}
+
+}
---
+++ Learn_Java_With_Dileep/src/test/java/com/java/dileep/LearnJavaWithDileepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.dileep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaWithDileepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
