9b34211d75a865bb4536824f932bab1303440558
Dileep-Develeper2309 <dileep.behera@outlook.com>
2024-10-02 12:37:39 +0530

    about page

---
+++ Learn_Java_With_Dileep/src/main/java/com/java/dileep/controller/AboutController.java
@@ -0,0 +1,13 @@
+package com.java.dileep.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AboutController {
+
+    @RequestMapping(value = "/about")
+    public String aboutPage() {
+        return "about.html";
+    }
+}
