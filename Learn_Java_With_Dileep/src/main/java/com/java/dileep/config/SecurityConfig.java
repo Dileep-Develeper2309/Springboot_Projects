aa26d17d0a2ef18a6301f33a5e088a9c6485c02a
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2024-10-22 02:37:49 +0530

    permet all to individual pages

---
+++ Learn_Java_With_Dileep/src/main/java/com/java/dileep/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.java.dileep.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        // Permit All Requests inside the Web Application
+        http.authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/about").permitAll()
+                        .requestMatchers("/assets/**").permitAll()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        // Deny All Requests inside the Web Application
+            /*http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());*/
+
+        return http.build();
+
+    }
+}
