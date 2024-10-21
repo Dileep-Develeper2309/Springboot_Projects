1d8cedbb6f290006a702e2f77e7eddb73955e6a6
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2024-10-22 02:45:26 +0530

    In Memory authorization

--- Learn_Java_With_Dileep/src/main/java/com/java/dileep/config/SecurityConfig.java
+++ Learn_Java_With_Dileep/src/main/java/com/java/dileep/config/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,19 +17,28 @@ public class SecurityConfig {
 
         // Permit All Requests inside the Web Application
         http.authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/", "/home").authenticated()
                         .requestMatchers("/about").permitAll()
                         .requestMatchers("/assets/**").permitAll()
                 )
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
-
-        // Deny All Requests inside the Web Application
-            /*http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());*/
-
         return http.build();
+    }
 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("54321")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
